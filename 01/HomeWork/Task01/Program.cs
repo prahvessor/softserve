2d6963f698b340b7efd2a2a7cb90e0ab15d35955
Emin Aliiev <emin.aliev@gmail.com>
2019-07-23 05:59:51 +0300

    task01 loops

---
+++ 01/HomeWork/Task01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Statistic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            // Case 1. User etners number of items, after that input numbers
+            int arrayLength = 0;
+            Console.WriteLine("Enter array length:");
+            arrayLength = int.Parse(Console.ReadLine());
+            //
+            Console.WriteLine("Enter numbers of array:");
+            int[] customArray = new int[arrayLength];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                customArray[i] = int.Parse(Console.ReadLine());
+            }
+            */
+            // Case 2. User inputs line of numbers
+            Console.WriteLine("Enter numbers. Use white space between numbers:");
+            String text = Console.ReadLine();
+            string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] customArray = new int[words.Length];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                customArray[i] = int.Parse(words[i]);
+            }
+
+            // using for loop
+            Console.WriteLine("Using For loop:");
+            int minIndexFor = Statistic.GetMinIndexByFor(customArray);
+            int maxIndexFor = Statistic.GetMaxIndexByFor(customArray);
+            Statistic.Print(customArray, minIndexFor, maxIndexFor);
+            
+            // using while loop
+            Console.WriteLine("Using While loop:");
+            int minIndexWhile = Statistic.GetMinIndexByWhile(customArray);
+            int maxIndexWhile = Statistic.GetMaxIndexByWhile(customArray);
+            Statistic.Print(customArray, minIndexWhile, maxIndexWhile);
+            
+            // using do loop
+            Console.WriteLine("Using Do loop:");
+            int minIndexDo = Statistic.GetMinIndexByDo(customArray);
+            int maxIndexDo = Statistic.GetMaxIndexByDo(customArray);
+            Statistic.Print(customArray, minIndexDo, maxIndexDo);
+        }
+    }
+}
---
+++ 01/HomeWork/Task01/Statistic.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistic
+{
+    class Statistic
+    {
+        public static int GetMinIndexByFor(int[] customArray)
+        {
+            int min = customArray[0];
+            int index = 0;
+            for (int i = 1; i < customArray.Length; i++)
+            {
+                if (min > customArray[i])
+                {
+                    min = customArray[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        public static int GetMaxIndexByFor(int[] customArray)
+        {
+            int max = customArray[0];
+            int index = 0;
+            for (int i = 1; i < customArray.Length; i++)
+            {
+                if (max < customArray[i])
+                {
+                    max = customArray[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        public static int GetMinIndexByWhile(int[] customArray)
+        {
+            int min = customArray[0];
+            int index = 0;
+            int i = 0;
+            while (i < customArray.Length)
+            {
+                if (min > customArray[i])
+                {
+                    min = customArray[i];
+                    index = i;
+                }
+                i++;
+            }
+            return index;
+        }
+
+        public static int GetMaxIndexByWhile(int[] customArray)
+        {
+            int max = customArray[0];
+            int index = 0;
+            int i = 0;
+            while (i < customArray.Length)
+            {
+                if (max < customArray[i])
+                {
+                    max = customArray[i];
+                    index = i;
+                }
+                i++;
+            }
+            return index;
+        }
+
+        public static int GetMinIndexByDo(int[] customArray)
+        {
+            int min = customArray[0];
+            int index = 0;
+            int i = 0;
+            do
+            {
+                if (min > customArray[i])
+                {
+                    min = customArray[i];
+                    index = i;
+                }
+                i++;
+            } while (i < customArray.Length);
+            return index;
+        }
+
+        public static int GetMaxIndexByDo(int[] customArray)
+        {
+            int max = customArray[0];
+            int index = 0;
+            int i = 0;
+            do
+            {
+                if (max < customArray[i])
+                {
+                    max = customArray[i];
+                    index = i;
+                }
+                i++;
+            } while (i < customArray.Length);
+            return index;
+        }
+
+        public static void Print(int[] customArray, int minIndex, int maxIndex)
+        {
+            Console.WriteLine("  Minimal element is customArray[{0}]={1}", minIndex, customArray[minIndex]);
+            Console.WriteLine("  Maximal element is customArray[{0}]={1}", maxIndex, customArray[maxIndex]);
+        }
+    }
+}
