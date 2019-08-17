d31e66de7e183d5427e5bba231d8611a8646479a
Emin Aliiev <emin.aliev@gmail.com>
2019-08-17 11:59:33 +0300

    added new operations

--- 01/HomeWork/Task03/Program.cs
+++ 01/HomeWork/Task03/Program.cs
@@ -27,36 +27,73 @@ namespace Task03
             
             Fraction fraction3 = fraction1 * fraction2;
             Console.WriteLine("{0} * {1} = {2}", fraction1, fraction2, fraction3);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Fraction fraction4 = fraction1 / fraction2;
             Console.WriteLine("{0} / {1} = {2}", fraction1, fraction2, fraction4);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Fraction fraction5 = fraction1 + fraction2;
             Console.WriteLine("{0} + {1} = {2}", fraction1, fraction2, fraction5);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Fraction fraction6 = fraction1 - fraction2;
             Console.WriteLine("{0} - {1} = {2}", fraction1, fraction2, fraction6);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("{0} > {1} is {2}", fraction1, fraction2, fraction1 > fraction2);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("{0} < {1} is {2}", fraction1, fraction2, fraction1 < fraction2);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("{0} == {1} is {2}", fraction1, fraction2, fraction1 == fraction2);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("{0} != {1} is {2}", fraction1, fraction2, fraction1 != fraction2);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("{0} >= {1} is {2}", fraction1, fraction2, fraction1 >= fraction2);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("{0} <= {1} is {2}", fraction1, fraction2, fraction1 <= fraction2);
+            Console.ReadKey();
+
+            Fraction fraction7 = new Fraction(0.5);
+            Fraction fraction8 = new Fraction(1.5);
+
+            Fraction fraction9 = fraction7 * fraction8;
+            Console.WriteLine("{0} * {1} = {2}", fraction7, fraction8, fraction9);
+            //Console.ReadKey();
+
+            Fraction fraction10 = fraction7 / fraction8;
+            Console.WriteLine("{0} / {1} = {2}", fraction7, fraction8, fraction10);
+            //Console.ReadKey();
+
+            Fraction fraction11 = fraction7 + fraction8;
+            Console.WriteLine("{0} + {1} = {2}", fraction7, fraction8, fraction11);
+            //Console.ReadKey();
+
+            Fraction fraction12 = fraction7 - fraction8;
+            Console.WriteLine("{0} - {1} = {2}", fraction7, fraction8, fraction12);
+            //Console.ReadKey();
+
+            Console.WriteLine("{0} > {1} is {2}", fraction7, fraction8, fraction7 > fraction8);
+            //Console.ReadKey();
+
+            Console.WriteLine("{0} < {1} is {2}", fraction7, fraction8, fraction7 < fraction8);
+            //Console.ReadKey();
+
+            Console.WriteLine("{0} == {1} is {2}", fraction7, fraction8, fraction7 == fraction8);
+            //Console.ReadKey();
+
+            Console.WriteLine("{0} != {1} is {2}", fraction7, fraction8, fraction7 != fraction8);
+            //Console.ReadKey();
+
+            Console.WriteLine("{0} >= {1} is {2}", fraction7, fraction8, fraction7 >= fraction8);
+            //Console.ReadKey();
+
+            Console.WriteLine("{0} <= {1} is {2}", fraction7, fraction8, fraction7 <= fraction8);
             Console.ReadKey();
         }
     }
